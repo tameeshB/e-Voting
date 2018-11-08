32a4ad5a7335e25bd86f05bb00791942171516d1
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-11-08 16:18:33 +0530

    Functional Tests with Chrome

--- tests/test_basic_init.py
+++ tests/test_basic_init.py
@@ -3,13 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest, time, re, os
 
 class BasicInit(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chromedriver = os.environ["webdriver_chrome"]
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        self.driver = webdriver.Chrome(chromedriver,chrome_options=chrome_options)
+        # self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "https://localhost:8000/"
         self.verificationErrors = []
@@ -27,7 +35,7 @@ class BasicInit(unittest.TestCase):
         driver.find_element_by_id("clientKey").clear()
         driver.find_element_by_id("clientKey").send_keys("a")
         driver.find_element_by_css_selector(".primary").click()
-        self.assertIn('Please log-in to proceed',self.browser.page_source)
+        self.assertIn('Please log-in to proceed',self.driver.page_source)
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
