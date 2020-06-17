994ea8fef398c3586ec9f1abcd83d8117eaf6d13
Tameesh Biswas <g@tameesh.in>
2020-06-17 07:31:06 +0530

    Works with single restart

--- evoting/settings.py
+++ evoting/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'USER': 'root', # os.environ.get('MYSQL_USER')
+        'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD'),
         'HOST': os.environ.get('MYSQL_HOST'),
         'PORT': '3306',
     }
